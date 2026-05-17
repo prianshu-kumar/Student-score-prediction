d120390acfd59694317181b3ae3a09745e2ca9db
prianshukumar <165042122+prianshukumar@users.noreply.github.com>
2026-05-17 17:37:22 +0530

    Fixed app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load model
-model = pickle.load(open("../model/model.pkl", "rb"))
+model = pickle.load(open("./model/model.pkl", "rb"))
 
 # Page config
 st.set_page_config(
